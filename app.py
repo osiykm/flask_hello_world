779091ee5e9bf0a2692f11586eefb75cbf682582
osiykm <osiykmukola@gmail.com>
2018-07-29 12:11:14 +0300

    add app module

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-@app.route('/index')
-def hello_world():
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+from app import app
