0dcc253ac705cebc87261cd59a5605b83507a878
osiykm <osiykmukola@gmail.com>
2018-07-29 12:15:26 +0300

    test string template

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,14 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'osiykm'}
+    return '''
+    <html>
+        <head>
+        <title>Test page</title>
+        <body>
+            <h1>Hello ''' + user['username'] + '''</h1>
+        </body>
+        </head>
+    </html>
+    '''
