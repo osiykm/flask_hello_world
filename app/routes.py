2aa2b07ca459e0543916fd2f2c1e91d2bfaaa4aa
osiykm <osiykmukola@gmail.com>
2018-07-29 12:42:46 +0300

    test for statement in templates

--- app/routes.py
+++ app/routes.py
@@ -7,4 +7,18 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'osiykm'}
-    return render_template('index.html', title='Test', user=user)
+    posts = [
+        {
+            'author': {'username': 'JoJo'},
+            'body': 'CHI NO SADAME!!!! JO!!! JO!!!'
+        },
+        {
+            'author': {'username': 'Dio'},
+            'body': 'It\' me Dio!!!'
+        },
+        {
+            'author': {'username': 'Speedwagon'},
+            'body': 'Fear'
+        },
+    ]
+    return render_template('index.html', title='Test', user=user, posts=posts)
